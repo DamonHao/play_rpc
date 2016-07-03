f2b0bbd5cf3b5e70d39082dc2edc6882e8cc6b2c
DamonHao <haolinknight@gmail.com>
2016-07-03 22:03:40 +0800

    add some base function

---
+++ script/net/net_address.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'damonhao'
+
+
+class NetAddress(object):
+	def __init__(self, address, port):
+		self.address = address
+		self.port = port
--- script/net/rpc/rpc_server.py
+++ script/net/rpc/rpc_server.py
@@ -6,31 +6,34 @@ __author__ = 'damonhao'
 
 from tornado import ioloop
 
-from tcp_server import TCPServer
+from tcp_server import TcpServer
 from codec import LengthHeaderCodec
 
 
-class RPCServer(object):
+class RpcServer(object):
 
-	def __init__(self):
-		self._server = TCPServer()
+	def __init__(self, io_loop=None):
+		self._server = TcpServer(io_loop)
 		self._codec = LengthHeaderCodec(self._on_message)
 		self._server.set_message_callback(self._codec.on_message)
 		self._server.set_connection_callback(self._on_connection)
 
 	def _on_message(self, conn, message):
-		print "RPCServer", conn.name, message
+		print "[RPCServer]_on_message", conn.name, message
 		pass
 
 	def _on_connection(self, conn):
-		print "LH _on_connection", conn.name, conn.is_connected
+		print "[RPCServer]_on_connection", conn.name, conn.is_connected
 
 	def listen(self, port, ip=""):
 		self._server.listen(port, ip)
 
+	def register_service(self, service):
+		pass
+
 
 if __name__ == '__main__':
-	server = RPCServer()
+	server = RpcServer()
 	server.listen(8002)
 	ioloop.IOLoop.instance().start()
 
